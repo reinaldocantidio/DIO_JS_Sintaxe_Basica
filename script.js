4d6847adc9ef621d8b04e269eaf83f24d2966292
reinaldocantidio <reinaldocantidio@gmail.com>
2022-09-07 11:57:00 -0300

    Conteúdo e material produzido no curso

---
+++ script.js
@@ -0,0 +1,44 @@
+/*
+function funcao1(){
+    console.log("Tudo correto");
+}
+
+funcao1();
+*/
+
+/*
+function mensagem(primeiroParametro, segundoParametro){
+    console.log(primeiroParametro, segundoParametro);
+}
+
+mensagem("Ola ", "tudo bem");
+*/
+/*
+function funcao2(){
+    console.log("Testando script");
+}
+
+funcao2();
+*/
+
+/*
+var funcao3 = function funcao3(){
+    console.log("mensage de funcao");
+}
+funcao3();
+*/
+
+/*
+var funcao3 = function (){
+    console.log("mensage de funcao");
+}
+funcao3();
+*/
+
+/*
+var funcao4 = ()=> {
+    console.log("teste de arrow function");
+}
+funcao4();
+*/
+
